dc0328265cfabc049f5da540c677fce2a8d04340
James Armes <jarmes@codeforamerica.org>
2025-02-28 16:28:33 -0500

    fix: Formatting

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket" {
-  value = aws_s3_bucket.tfstate.id
+  value       = aws_s3_bucket.tfstate.id
   description = "The S3 bucket used to store the Terraform state."
 }
 
 output "kms_key" {
-  value = aws_kms_key.backend.id
+  value       = aws_kms_key.backend.id
   description = "The KMS key used to encrypt the Terraform state."
 }
